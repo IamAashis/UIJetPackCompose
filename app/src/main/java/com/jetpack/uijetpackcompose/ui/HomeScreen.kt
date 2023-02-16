7dbc61dc997c86925a1f54d038ea7a512d9416c0
“Aashis” <thapaaashis20@gmail.com>
2023-02-16 13:29:27 +0545

    Update design

--- app/src/main/java/com/jetpack/uijetpackcompose/ui/HomeScreen.kt
+++ app/src/main/java/com/jetpack/uijetpackcompose/ui/HomeScreen.kt
@@ -1,6 +1,5 @@
 package com.jetpack.uijetpackcompose.ui
 
-import android.widget.ImageView
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
@@ -11,21 +10,21 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.jetpack.uijetpackcompose.R
+import com.jetpack.uijetpackcompose.ui.model.Experience
 import com.jetpack.uijetpackcompose.ui.model.Features
 import com.jetpack.uijetpackcompose.ui.theme.ColorBoxBorder
 import com.jetpack.uijetpackcompose.ui.theme.ColorWhite
@@ -48,7 +47,71 @@ fun HomeScreen() {
                     Features("Restaurant", R.drawable.restaurant)
                 )
             )
+            TopRatedExperience(
+                listOf(
+                    Experience("This is title", "", R.drawable.houses, false, "", "")
+                )
+            )
         }
+    }
+}
+
+@Composable
+fun TopRatedExperience(listOfExperience: List<Experience>) {
+    Text(
+        text = "Top-rated experiences",
+        modifier = Modifier.padding(start = 20.dp),
+        fontSize = 21.sp,
+        fontWeight = FontWeight.Bold
+    )
+
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        contentPadding = PaddingValues(start = 7.5.dp, end = 7.5.dp, bottom = 100.dp),
+        modifier = Modifier.fillMaxHeight()
+    ) {
+        items(listOfExperience.size) {
+            ExperienceSection(listOfExperience[it])
+        }
+    }
+}
+
+@Composable
+fun ExperienceSection(experience: Experience) {
+    Column() {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(15.dp)
+        ) {
+            Image(
+                painter = painterResource(id = experience.iconId), contentDescription = "My Image"
+            )
+            Icon(
+                painter = painterResource(id = R.drawable.ic_favorite),
+                contentDescription = experience.title,
+                tint = Color.White,
+                modifier = Modifier.align(Alignment.BottomStart)
+            )
+        }
+        Text(
+            text = "What can we help you find, Sandor?",
+            color = Color.Gray,
+            modifier = Modifier.padding(15.dp),
+            fontSize = 15.sp
+        )
+        Text(
+            text = "What can we help you find, Sandor?",
+            color = Color.Black,
+            modifier = Modifier.padding(15.dp),
+            fontSize = 20.sp, fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = "What can we help you find, Sandor?",
+            color = Color.Blue,
+            modifier = Modifier.padding(15.dp),
+            fontSize = 20.sp, fontWeight = FontWeight.Bold
+        )
     }
 }
 
@@ -61,7 +124,7 @@ fun SearchSection() {
         onValueChange = { text.value = it },
         leadingIcon = {
             Icon(
-                painter = painterResource(id = com.jetpack.uijetpackcompose.R.drawable.ic_search),
+                painter = painterResource(id = R.drawable.ic_search),
                 contentDescription = "Back button"
             )
         },
@@ -84,7 +147,9 @@ fun ChipSection(types: List<String>) {
             Box(modifier = Modifier
                 .padding(start = 10.dp)
                 .clickable {}
-                .border(BorderStroke(1.dp, ColorBoxBorder), shape = RoundedCornerShape(2.dp))
+                .border(
+                    BorderStroke(1.dp, ColorBoxBorder), shape = RoundedCornerShape(2.dp)
+                )
                 .background(ColorWhite)
                 .padding(7.dp)) {
                 Text(text = types[it], color = Color.Black)
@@ -96,15 +161,14 @@ fun ChipSection(types: List<String>) {
 @ExperimentalFoundationApi
 @Composable
 fun FeatureSection(features: List<Features>) {
-    Column(modifier = Modifier.fillMaxWidth()) {
+    Column() {
         Text(
-            text = "What can we help you find, Sandor?",
-            modifier = Modifier.padding(15.dp),
+            text = "What can we help you find, Sandor?", modifier = Modifier.padding(15.dp)
         )
         LazyHorizontalGrid(
             rows = GridCells.Fixed(1),
-            contentPadding = PaddingValues(start = 7.5.dp, end = 7.5.dp),
-//            modifier = Modifier.fillMaxHeight()
+            contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp),
+            modifier = Modifier.height(150.dp)
         ) {
             items(features.size) {
                 FeatureItem(feature = features[it])
@@ -116,18 +180,23 @@ fun FeatureSection(features: List<Features>) {
 @Composable
 fun FeatureItem(feature: Features) {
 
-    Box(modifier = Modifier
-        .padding(start = 10.dp)
-        .clickable {}
-        .border(BorderStroke(1.dp, ColorBoxBorder), shape = RoundedCornerShape(2.dp))
+    Box(
+        modifier = Modifier.padding(end = 20.dp)
     ) {
         Column() {
             Image(
                 painter = painterResource(feature.iconId),
                 contentDescription = "My Image",
-                modifier = Modifier.fillMaxWidth()
+//                modifier = Modifier.fillMaxWidth()
             )
-            Text(text = feature.title, color = Color.Black)
+            Box(
+//                modifier = Modifier
+//                    .fillMaxWidth()
+            ) {
+                Text(
+                    text = feature.title, modifier = Modifier.fillMaxWidth(), color = Color.Black
+                )
+            }
         }
     }
 }
---
+++ app/src/main/java/com/jetpack/uijetpackcompose/ui/model/Experience.kt
@@ -0,0 +1,15 @@
+package com.jetpack.uijetpackcompose.ui.model
+
+import androidx.annotation.DrawableRes
+
+/**
+ * Created by Aashis on 16,February,2023
+ */
+data class Experience(
+    val title: String,
+    val subTitle: String,
+    @DrawableRes val iconId: Int,
+    val fav: Boolean,
+    var sizes: String,
+    var rating: String
+)
